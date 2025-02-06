fd598ca241c114018bb3f1a1deab0b2b82e4297b
vatsalvyas1 <vatsalvyas4@gmail.com>
2025-02-06 22:27:21 +0530

    footer name edit

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
       <footer className="bg-gray-800 text-white py-4">
         <div className="container mx-auto text-center">
-          <p>&copy; {new Date().getFullYear()} Samaritan. All Rights Reserved.</p>
+          <p>&copy; {new Date().getFullYear()} Darpan. All Rights Reserved.</p>
         </div>
       </footer>
     );
